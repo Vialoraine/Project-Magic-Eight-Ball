1875a54cd4e0c3106c92459d3183c44fcb76579e
Loreina Salalila <vialoraine.gg@gmail.com>
2024-10-15 02:51:22 -0700

    task 6 completed

--- app.js
+++ app.js
@@ -7,3 +7,5 @@ const userQuestion = '"Will I win the lottery?"';
 userName ? console.log(`${userName} asks the Magic 8 ball: ${userQuestion}`) : console.log(`You ask the Magic 8 ball: ${userQuestion}`);
 
 let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
