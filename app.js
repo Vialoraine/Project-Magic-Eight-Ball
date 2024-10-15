c05ee996c16946790f912be3a3e55cb461583b5b
Loreina Salalila <vialoraine.gg@gmail.com>
2024-10-15 02:50:25 -0700

    task 4 completed

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ let userName = "Loraine";
 userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 
 const userQuestion = '"Will I win the lottery?"';
+
+userName ? console.log(`${userName} asks the Magic 8 ball: ${userQuestion}`) : console.log(`You ask the Magic 8 ball: ${userQuestion}`);
